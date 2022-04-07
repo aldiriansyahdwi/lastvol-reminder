fb64180534deb3d4ce524a31df4e61c59d21e386
Aldiriansyah Dwi <kurosenpai12@gmail.com>
2022-04-07 13:41:04 +0700

    add delete button function on click

--- app/src/main/java/com/example/lastvolreminder/BookAdapter.kt
+++ app/src/main/java/com/example/lastvolreminder/BookAdapter.kt
@@ -1,15 +1,20 @@
 package com.example.lastvolreminder
 
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lastvolreminder.bookdatabase.Book
+import com.example.lastvolreminder.bookdatabase.BookDatabase
 import com.example.lastvolreminder.databinding.BookItemBinding
 import com.example.lastvolreminder.databinding.FragmentEditBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 class BookAdapter(val listBook: List<Book>): RecyclerView.Adapter<BookAdapter.ViewHolder>() {
 
@@ -32,6 +37,27 @@ class BookAdapter(val listBook: List<Book>): RecyclerView.Adapter<BookAdapter.Vi
                     val activity = it.context as MainActivity
                     val dialogFragment = EditFragment(listBook[position])
                     dialogFragment.show(activity.supportFragmentManager, null)
+                }
+
+                binding.btnDelete.setOnClickListener {
+                    AlertDialog.Builder(it.context).setPositiveButton("Yes"){ p0, p1 ->
+                        val bookDb = BookDatabase.getInstance(holder.itemView.context)
+
+                        GlobalScope.async{
+                            val result = bookDb?.bookDao()?.deleteBook(listBook[position])
+
+                            (holder.itemView.context as MainActivity).runOnUiThread {
+                                if (result!= 0)
+                                    Toast.makeText(it.context, "Data \"${listBook[position].title}\" deleted", Toast.LENGTH_SHORT).show()
+                                else
+                                    Toast.makeText(it.context, "Data \"${listBook[position].title} \"failed to delete", Toast.LENGTH_SHORT).show()
+                            }
+                            (holder.itemView.context as ListFragment).fetchData()
+                        }
+                    }.setNegativeButton("Cancel"){
+                        p0, p1 ->
+                        p0.dismiss()
+                    }.setMessage("Are you sure to delete \"${listBook[position].title}\" ?").setTitle("Delete Confirmation").create().show()
                 }
             }
 
